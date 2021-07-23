429a25b5f74ea5096f55e2dc47a500a1252e9c57
Alexandre Péré <alexandre.pere@zama.ai>
2021-07-23 16:24:31 +0200

    fix(concrete_fftw_sys): fixes broken crates
    The CI of the concrete repository was broken because of issues in
    the concrete-fftw crates, which are fixed by this commit.
    
    Using `thiserror` as a dependency brings the `syn` crate in the
    dependencies, which is incompatible with the `-Cpanic=abort` rustflag.
    This rustflag was necessary to compute code coverage though. This
    dependency removed this dependency and standard error management was
    implemented.
    
    Also, the folder containing the sources of fftw contianed a gitignore
    file which for some reasons, made the compilation fail with the avx
    flags. The gitignore file was removed, making the compilation operational
    again.
    
    Moreover, it was noticed that when `concrete-fftw` was not depending on the
    local version of `concrete-fftw-sys`, the CI was not even building the
    current version of `concrete-fftw-sys`. This was the main reason why
    the bugs were only caught in the concrete CI and not the concrete-fftw
    CI. The CI was fixed to build all the packages in the repository.
    
    Lastly, we synchronized the versions of `concrete-fftw` with the current
    `concrete-fftw-sys` to prepare for a new release.

--- concrete-fftw/src/error.rs
+++ concrete-fftw/src/error.rs
@@ -1,22 +1,41 @@
 use crate::array::Alignment;
-use thiserror::Error;
+use std::fmt::{Display, Formatter};
 
 pub type Result<T> = ::std::result::Result<T, Error>;
 
-#[derive(Debug, Error)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("Invalid Plan")]
     InvalidPlanError {},
-
-    #[error("Input array mismatch: expect={:?}, actual={:?}", expect, actual)]
     InputArrayMismatch {
         expect: (usize, Alignment),
         actual: (usize, Alignment),
     },
-
-    #[error("Output array mismatch: expect={:?}, actual={:?}", expect, actual)]
     OutputArrayMismatch {
         expect: (usize, Alignment),
         actual: (usize, Alignment),
     },
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::InvalidPlanError {} => write!(f, "Invalid Plan"),
+            Error::OutputArrayMismatch { expect, actual } => {
+                write!(
+                    f,
+                    "Output array mismatch: expect={:?}, actual={:?}",
+                    expect, actual
+                )
+            }
+            Error::InputArrayMismatch { expect, actual } => {
+                write!(
+                    f,
+                    "Input array mismatch: expect={:?}, actual={:?}",
+                    expect, actual
+                )
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
